d522f601d3bfb1b25fa6cab8c1627b0eba5f6a92
Joraaaan <149679322+Joraaaan@users.noreply.github.com>
2024-02-15 10:56:00 +0100

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,20 @@
+-- TABLE
+CREATE TABLE Calendrier
+(idCalendrier INT PRIMARY KEY NOT NULL,
+ titre DEFAULT (255),
+ description DEFAULT (255),
+ event_date DATE DEFAULT(255));
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Utilisateur
+(idUtilisateur INT PRIMARY KEY NOT NULL,
+ prenom DEFAULT(100),
+ nom DEFAULT(100),
+ email DEFAULT(255),
+ mot de passe DEFAULT(255));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
