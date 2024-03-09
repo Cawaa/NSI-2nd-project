e4b5daba12b0adcbc24cd320f788ce1bfc5d2f0e
Joraaaan <149679322+Joraaaan@users.noreply.github.com>
2024-03-09 11:16:02 +0100

    Update dump.sqlite.sql

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -4,13 +4,17 @@ CREATE TABLE Calendrier
  titre DEFAULT (255),
  description DEFAULT (255),
  event_date DATE DEFAULT(255));
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+
 CREATE TABLE Utilisateur
 (idUtilisateur INT PRIMARY KEY NOT NULL,
  prenom DEFAULT(100),
  nom DEFAULT(100),
  email DEFAULT(255),
  mot de passe DEFAULT(255));
+
+SELECT titre
+FROM Calendrier
+INNER JOIN Utilisateur ON idCalendrier = idUtilisateur
  
 -- INDEX
  
